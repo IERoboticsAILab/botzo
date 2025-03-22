3216a3002cea4a98d1a27be4fa6a4e21a5cf0420
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2025-03-22 18:55:58 +0100

    BR leg working

--- src/IK_solver.py
+++ src/IK_solver.py
@@ -8,6 +8,58 @@ tibia = 9.8 # from knee to foot
 real_femur = 9.1 # lenght of 3D printed femur
 dist_focuspoint_servo_femurtibia = 2.8 # distance from focus point/pivot of the 2 servos in the shoulder
 
+''' HARD CODED TRAJECTORIES '''
+home = [[0,2,16]]
+sleep = [[0,2,13],[0,2,10],[0,2,8]]
+jump = [[0,2,8],[-1,2,16],[0,2,9],[0,2,16]]
+forward_targets_FR_BL = [
+    [0,3,15],[0.5,3,15],[1,3,15],[1.5,3,15],[2,3,15],[2.5,3,15],[3,3,15],[3.5,3,15],[4,3,15],
+    [4,3,14],[4,3,13],[4,3,12],
+    [3,3,12],[2,3,12],[1,3,12],[0,3,12],
+    [0,3,13],[0,3,14]
+]
+forward_targets_FL_BR = [
+    [4,3,15],[4,3,14],[4,3,13],[4,3,12],
+    [3,3,12],[2,3,12],[1,3,12],[0,3,12],
+    [0,3,13],[0,3,14],[0,3,15],
+    [0.5,3,15],[1,3,15],[1.5,3,15],[2,3,15],[2.5,3,15],[3,3,15],[3.5,3,15]
+]
+backward_targets_FR_BL = [
+    [0,3,15],[0,3,14],[0,3,13],[0,3,12],
+    [1,3,12],[2,3,12],[3,3,12],[4,3,12],
+    [4,3,13],[4,3,14],[4,3,15],
+    [3.5,3,15],[3,3,15],[2.5,3,15],[2,3,15],[1.5,3,15],[1,3,15],[0.5,3,15]
+]
+backward_targets_FL_BR = [
+    [4,3,15],[3.5,3,15],[3,3,15],[2.5,3,15],[2,3,15],[1.5,3,15],[1,3,15],[0.5,3,15],
+    [0,3,15],[0,3,14],[0,3,13],[0,3,12],
+    [1,3,12],[2,3,12],[3,3,12],[4,3,12],
+    [4,3,13],[4,3,14]
+]
+spin_left_targets_FR_BL = [
+    [0,3,15],[0,2.5,15],[0,2,15],[0,1.5,15],[0,1,15],[0,0.5,15],[0,0,15],[0,-0.5,15],[0,-1,15],
+    [0,-1,14],[0,-1,13],[0,-1,12],
+    [0,0,12],[0,1,12],[0,2,12],[0,3,12],
+    [0,3,13],[0,3,14]
+]
+spin_left_targets_FL_BR = [
+    [0,3,15],[0,3,14],[0,3,13],[0,3,12],
+    [0,2,12],[0,1,12],[0,0,12],[0,-1,12],
+    [0,-1,13],[0,-1,14],[0,-1,15],
+    [0,-0.5,15],[0,0,15],[0,0.5,15],[0,1,15],[0,1.5,15],[0,2,15],[0,2.5,15]
+]
+spin_right_targets_FR_BL = [
+    [0,3,15],[0,3,14],[0,3,13],[0,3,12],
+    [0,2,12],[0,1,12],[0,0,12],[0,-1,12],
+    [0,-1,13],[0,-1,14],[0,-1,15],
+    [0,-0.5,15],[0,0,15],[0,0.5,15],[0,1,15],[0,1.5,15],[0,2,15],[0,2.5,15]
+]
+spin_right_targets_FL_BR = [
+    [0,3,15],[0,2.5,15],[0,2,15],[0,1.5,15],[0,1,15],[0,0.5,15],[0,0,15],[0,-0.5,15],[0,-1,15],
+    [0,-1,14],[0,-1,13],[0,-1,12],
+    [0,0,12],[0,1,12],[0,2,12],[0,3,12],
+    [0,3,13],[0,3,14]
+]
 
 def real_sim_angle(angle, id):
   if id == 7: #  BR_femur_joint
@@ -23,57 +75,20 @@ def rad2deg(rads):
 def deg2rad(deg):
   return deg*np.pi/180
 
-def FR_legIK(x,y,z):
-  D = np.sqrt((z**2 + y**2) - coxa**2)
-  G = np.sqrt(D**2 + x**2)
-  knee_angle = np.arccos((G**2 - femur**2 - tibia**2)/(-2*femur*tibia))
-  shoulder_angle = np.arctan2(x,D) + np.arcsin((tibia * np.sin(knee_angle)) / G)
-  adjustment = np.arccos((real_femur**2 + femur**2 - dist_focuspoint_servo_femurtibia**2) / (2 * real_femur * femur))
 
-  coxa_angle = deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa))
-  femur_angle = deg2rad(90) - (shoulder_angle + adjustment)
-  tibia_angle = np.pi - knee_angle + femur_angle + adjustment
 
-  return rad2deg([coxa_angle, femur_angle, tibia_angle])
+
+
+'''
+X (forward(neg), backward(pos)):    |   Y (inside(neg), outside(pos)):      |   Z (taller(neg), shorter(pos)):   |
+'''
 
 def BR_legIK(x,y,z):
   D = np.sqrt((z**2 + y**2) - coxa**2)
   G = np.sqrt(D**2 + x**2)
+  shoulder_angle = np.arctan2(y,z) + np.arctan2(D,coxa)
   knee_angle = np.arccos((G**2 - femur**2 - tibia**2)/(-2*femur*tibia))
-  shoulder_angle = np.arctan2(x,D) + np.arcsin((tibia * np.sin(knee_angle)) / G)
+  femur_angle = deg2rad(180) - (deg2rad(90) + np.arctan2(x,D) + np.arcsin((tibia * np.sin(knee_angle)) / G))
 
-  coxa_angle = deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa))
-  femur_angle = deg2rad(90) - (shoulder_angle)
-  tibia_angle = np.pi - knee_angle + femur_angle
+  return rad2deg([shoulder_angle, femur_angle, knee_angle])
 
-  return rad2deg([coxa_angle, femur_angle, tibia_angle])
-
-def FL_legIK(x,y,z):
-  D = np.sqrt((z**2 + y**2) - coxa**2)
-  G = np.sqrt(D**2 + x**2)
-  knee_angle = np.arccos((G**2 - femur**2 - tibia**2)/(-2*femur*tibia))
-  shoulder_angle = np.arctan2(x,D) + np.arcsin((tibia * np.sin(knee_angle)) / G)
-  adjustment = np.arccos((real_femur**2 + femur**2 - dist_focuspoint_servo_femurtibia**2) / (2 * real_femur * femur))
-
-  coxa_angle = (deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa))) - (2 * ((deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa)))-deg2rad(90)))
-  femur_angle = deg2rad(90) - (shoulder_angle + adjustment)
-  tibia_angle = np.pi - knee_angle + femur_angle + adjustment
-
-  femur_angle = deg2rad(180) - femur_angle
-  tibia_angle = deg2rad(180) - tibia_angle
-
-  return rad2deg([coxa_angle, femur_angle, tibia_angle])
-
-def BL_legIK(x,y,z):
-  D = np.sqrt((z**2 + y**2) - coxa**2)
-  G = np.sqrt(D**2 + x**2)
-  knee_angle = np.arccos((G**2 - femur**2 - tibia**2)/(-2*femur*tibia))
-  shoulder_angle = np.arctan2(x,D) + np.arcsin((tibia * np.sin(knee_angle)) / G)
-  adjustment = np.arccos((real_femur**2 + femur**2 - dist_focuspoint_servo_femurtibia**2) / (2 * real_femur * femur))
-
-  coxa_angle = ((deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa))) - (2 * ((deg2rad(180) - (np.arctan2(y,z) + np.arctan2(D,coxa)))-deg2rad(90)))) - deg2rad(5.5)
-  femur_angle = deg2rad(90) - (shoulder_angle + adjustment)
-  tibia_angle = deg2rad(180) - (np.pi - knee_angle + femur_angle + adjustment)
-  femur_angle = deg2rad(180) - femur_angle
-
-  return rad2deg([coxa_angle, femur_angle, tibia_angle])
