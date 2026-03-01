71db0ae083ae1e31dbf07e11b12c58c2a5c4a5b9
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2026-03-01 21:40:11 +0100

    change joints names

--- botzo_ros2_ws/src/move_joint.py
+++ botzo_ros2_ws/src/move_joint.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 
+
 class JointPublisher(Node):
     def __init__(self):
         super().__init__('joint_publisher')
@@ -11,40 +12,38 @@ class JointPublisher(Node):
         self.joint_state = JointState()
         '''
         NAMES:
-        - LH_HAA
-        - RH_HAA
-        - RF_HAA
-        - LF_HAA
-        - LF_HFE
-        - RF_HFE
-        - RH_HFE
-        - LH_HFE
-        - LF_KFE
-        - RF_KFE
-        - LH_KFE
-        - RH_KFE
-        - RH_shank_fixed_RH_FOOT
-        - LH_shank_fixed_LH_FOOT
-        - LF_shank_fixed_LF_FOOT
-        - RF_shank_fixed_RF_FOOT
+        - BL_shoulder_joint
+        - BR_shoulder_joint
+        - FL_shoulder_joint
+        - FR_shoulder_joint
+        - FR_femur_joint
+        - FR_tibia_joint
+        - FL_femur_joint
+        - FL_tibia_joint
+        - BR_femur_joint
+        - BR_tibia_joint
+        - BL_femur_joint
+        - BL_tibia_joint
         '''
-        self.joint_state.name = ['LH_HAA', 'RH_HAA', 'RF_HAA', 'LF_HAA', 'LF_HFE', 'RF_HFE', 'RH_HFE', 'LH_HFE',
-                                 'LF_KFE', 'RF_KFE', 'LH_KFE', 'RH_KFE', 'RH_shank_fixed_RH_FOOT', 'LH_shank_fixed_LH_FOOT',
-                                 'LF_shank_fixed_LF_FOOT', 'RF_shank_fixed_RF_FOOT']
-        self.joint_state.position = [0.0] * 16
-        self.joint_state.velocity = [0.0] * 16
-        self.joint_state.effort = [0.0] * 16
+        self.joint_state.name = ['BL_shoulder_joint', 'BR_shoulder_joint', 'FL_shoulder_joint', 'FR_shoulder_joint',
+                                 'FR_femur_joint', 'FR_tibia_joint', 'FL_femur_joint', 'FL_tibia_joint',
+                                 'BR_femur_joint', 'BR_tibia_joint', 'BL_femur_joint', 'BL_tibia_joint']
+        self.joint_state.position = [0.0] * 12
+        self.joint_state.velocity = [0.0] * 12
+        self.joint_state.effort = [0.0] * 12
+
 
     def publish_joints(self):
         self.joint_state.header.stamp = self.get_clock().now().to_msg()
         self.joint_state.position = [1.0, 0.5, 0.0, 0.0, 1.0, 0.5, 0.0, 0.0,
-                                     1.0, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0]
+                                     1.0, 0.5, 1.0, 0.5]
         self.joint_state.velocity = [1.0, 0.5, 1.0, 1.0, 1.0, 1.5, 2.5, -2.5,
-                                     -2.5, -2.5, -2.5, -2.5, 0.0, 0.0, 0.0, 0.0]
+                                     -2.5, -2.5, -2.5, -2.5]
         self.joint_state.effort = [2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0,
-                                   2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0]
+                                   2.0, 2.0, 2.0, 2.0]
         print(f'Publishing joint states: {self.joint_state}')
         self.publisher.publish(self.joint_state)
+
 
 def main():
     rclpy.init()
--- docs/build/software/ros.md
+++ docs/build/software/ros.md
@@ -1,1 +1,17 @@
 # ROS2 Jazzy Botzo Workspace Documentation
+
+Build Botzo Workspace for ROS2 Jazzy
+
+```bash
+cd <your_ros2_ws>/src
+git clone https://github.com/IERoboticsAILab/botzo.git
+cd ..
+colcon build
+source install/setup.bash
+```
+
+Launch RViz Botzo Description
+
+```bash
+ros2 launch botzo_ros2_ws display.launch.py
+```
